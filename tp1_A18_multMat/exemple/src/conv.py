edb6f3a5827695789c3c780b749b0bf3e8daf731
Salim Badirou <salimbad2011@hotmail.fr>
2018-10-08 14:50:17 -0400

    Correctif algo strassen et conventionnel

--- tp1_A18_multMat/exemple/src/conv.py
+++ tp1_A18_multMat/exemple/src/conv.py
@@ -1,12 +1,71 @@
 import sys
+import time
 
-ex_path1 = sys.argv[1] # Path de la première matrice
-ex_path2 = sys.argv[2] # Path de la deuxième matrice
+# Permet la lecture d'une matrice
+# A besoin de path du fichier
 
-# TODO: Algo de multiplication de matrices ici (dans le cas de conv.py, l'algo conventionnel)
-
+fileName1 = str(sys.argv[1])
+fileName2 = str(sys.argv[2])
 options = sys.argv[3:]
-if '-p' in options: # On imprime la matrice résultat
-    print("2\n1\t2\t3\t4\n5\t6\t7\t8\n9\t10\t11\t12\n13\t14\t15\t16") # Données bidon, mais output du bon format demandé
-if '-t' in options: # On imprime le temps d'exécution
-    print("4.1347628746") # Données bidon, mais output du bon format demandé
+
+# fonction pour imprimer une matrice
+def printMatrice(matrice, N):
+    for i in range(N):
+        line = ''
+        for j in range(N):
+            line += str(matrice[i][j]) + '\t'
+        print(line)
+
+fichier1 = open(fileName1, 'r')
+fichier2 = open(fileName2, 'r')
+
+expoMatrice1 = int(fichier1.readline())
+expoMatrice2 = int(fichier2.readline())
+if expoMatrice1 == expoMatrice2:
+    N = expoMatrice1*2
+
+    matrice1 = [None]*N
+    matrice2 = [None]*N
+
+    for i in range(N):
+        line1 = fichier1.readline().replace('\t\n', '')
+        matrice1[i] = line1.split('\t')
+
+        line2 = fichier2.readline().replace('\t\n', '')
+        matrice2[i] = line2.split('\t')
+
+    #
+    ## Debut Algo Conventionnel
+    #
+    start_time = time.time()
+
+    result = [[0 for x in range(N)] for y in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                result[i][j] += int(matrice1[i][k]) * int(matrice2[k][j])
+
+    end_time = time.time()
+
+    if '-all' in options:  # On imprime la matrice résultat
+        print('Matrice 1 :')
+        printMatrice(matrice1, N)
+
+        print('\n' + 'Matrice 2 :')
+        printMatrice(matrice2, N)
+
+        print('\n' + 'Result :')
+        printMatrice(result, N)
+
+    if '-p' in options:  # On imprime la matrice résultat
+        print('Result :')
+        printMatrice(result, N)
+
+    if '-t' in options:  # On imprime le temps d'exécution
+        interval = end_time - start_time
+        print('\n' + 'Temps d execution :' + str(interval) + ' sec')
+
+else:
+    print('ERREUR !!!')
+    print('Les deux matrices sont de taille differente')
