dfb86d19f2022015ae0da2158a0788063036da65
Salim Badirou <salimbad2011@hotmail.fr>
2018-10-08 15:08:47 -0400

    correction du path des series

--- tp1_A18_multMat/exemple/autotests.sh
+++ tp1_A18_multMat/exemple/autotests.sh
@@ -8,7 +8,7 @@ for algo in {"conv","strassen","strassenSeuil"}; do
 	counter=4
 	for ex1 in $(ls "exemplaires/$serie"); do
 	    for ex2 in $(ls "exemplaires/$serie" | tail -n $counter); do
-		t=$(./tp.sh -a $algo -e $serie/$ex1 $serie/$ex2 -t)
+		t=$(./tp.sh -a $algo -e exemplaires/$serie/$ex1 exemplaires/$serie/$ex2 -t)
 		echo $t >> ./resultats/${algo}_${serie}.csv
 	    done
 	    ((counter--))
