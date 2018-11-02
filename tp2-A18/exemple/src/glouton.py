04cc1a81dc3367cee11f1516596b491bf479b80b
wabad <bissowaj@yahoo.fr>
2018-11-02 14:07:51 -0400

    WB: finish the glouton algo

--- tp2-A18/exemple/src/glouton.py
+++ tp2-A18/exemple/src/glouton.py
@@ -1,6 +1,8 @@
 import sys
+import time
 
 ex_path = sys.argv[1] # Path de l'exemplaire
+options = sys.argv[2:]
 
 # Lecture du fichier source
 fichier = open(ex_path,'r')
@@ -12,8 +14,29 @@ for i in range(taille):
     batons.append(int(data[1]))
 poidTotal = int(fichier.readline())
 
-options = sys.argv[2:]
-if '-p' in options: # On imprime la solution
-    print("84 73 12 44 98 75") # Données bidon, mais output du bon format demandé
-if '-t' in options: # On imprime le temps d'exécution
-    print("4.1347628746") # Données bidon, mais output du bon format demandé
+def glouton(batons, poidsMax,taille):
+    resultat = []
+    poids = 0
+    i = 1
+    if batons[0] < poidsMax :
+        resultat.append(batons[0])
+        poids += batons[0]
+    while i <= taille:
+        if poids + batons[i] <= poidsMax :
+            resultat.append(batons[i])
+            poids += batons[i]    
+        elif poids == poidsMax :
+            break
+        i += 1
+    return resultat
+
+start_time = time.time()
+result = glouton(batons,poidTotal,taille)
+end_time = time.time()
+    
+if '-p' in options:  # On imprime la matrice résultat
+    print(result) # Données bidon, mais output du bon format demandé
+if '-t' in options:  # On imprime le temps d'exécution
+    interval = end_time - start_time
+    print(interval)
+    #print('\n' + 'Temps d execution :' + str(interval) + ' sec')
