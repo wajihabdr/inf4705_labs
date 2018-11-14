4795bbf6fb7f3d3c21cad4d3463f193635cd0363
Salim Badirou <salimbad2011@hotmail.fr>
2018-11-13 22:49:49 -0500

    petite customization bonus

--- tp2-A18/exemple/autotestAll.sh
+++ tp2-A18/exemple/autotestAll.sh
@@ -1,15 +1,16 @@
 #!/bin/bash:
 
-algo="glouton"
-
-for directory in $(ls "../jeux_de_donnees"); do
-    echo "Go ----> ${directory}"
-    echo "--  ${directory} --" >> ./resultats/${algo}.csv
-    for file in $(ls "../jeux_de_donnees/$directory"); do
-        t=$(./tp.sh -a $algo -e ../jeux_de_donnees/$directory/$file -test)
-        echo $t >> ./resultats/${algo}.csv
+for algo in $(ls "src"); do
+    echo "Go ----> ${algo}"
+    echo
+    for directory in $(ls "../jeux_de_donnees"); do
+        echo "Go ----> ${directory}"
+        echo "--  ${directory} --" >> ./resultats/${algo}.csv
+        for file in $(ls "../jeux_de_donnees/$directory"); do
+            t=$(./tp.sh -a ${algo%%.*} -e ../jeux_de_donnees/$directory/$file -test)
+            echo $t >> ./resultats/${algo}.csv
+        done
     done
-    read -p "Appuyer sur une touche pour continuer ..."
 done
 
 echo Fin
